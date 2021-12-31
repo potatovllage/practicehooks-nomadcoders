3e9caa75d0c83bf540e880666a7989a1cd93adf5
Yeong joon <bluehome8626@naver.com>
2021-12-31 10:58:45 +0900

    노마드 코더 react hooks 강의

---
+++ src/components/lecture1.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+function lecture1() {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+
+  return (
+    <div>
+      <h1>Hello {item}</h1>
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>Decrement</button>
+    </div>
+  );
+}
+export default lecture1;
