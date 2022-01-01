a7811d14df4c038020c04c28e684071bbdb16e4c
Yeong joon <bluehome8626@naver.com>
2022-01-01 22:12:29 +0900

    [UPDATE] useConfirm

--- src/lecture1.jsx
+++ src/lecture1.jsx
@@ -1,26 +1,26 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const useClick = (onClick) => {
-  const element = useRef();
-  useEffect(() => {
-    if (element.current) {
-      element.current.addEventListener("click", onClick);
+const useConfirm = (message = "", callback) => {
+  if (typeof callback !== "function") {
+    return;
+  }
+  const confirmAction = () => {
+    if (confirm(message)) {
+      callback();
+    } else {
+      rejection();
     }
-    return () => {
-      if (element.current) {
-        element.current.removeEventListener("click", onClick);
-      }
-    };
-  }, []);
-  return element;
+  };
+  return confirmAction;
 };
 
 function lecture1() {
-  const sayHello = () => console.log("say hello");
-  const title = useClick();
+  const deleteWorld = () => console.log("Deleting the  world");
+  const abort = () => console.log("Aborted");
+  const confirmDelete = useConfirm("Are you sure", deleteWorld);
   return (
     <div>
-      <div>hi</div>
+      <button onClick={confirmDelete}>Delete the world</button>
     </div>
   );
 }
